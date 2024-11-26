09c793daeee043aa3124ba0efc437822e68fc53c
Joel Ymele <jymele@yahoo.fr>
2024-11-26 16:52:38 -0500

    Working on meal page

--- app/(toolbar)/meal/page.tsx
+++ app/(toolbar)/meal/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+import { Calendar } from "@/components/ui/calendar";
 
 export default function Page() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
   return (
     <div>
       <h1>Meal</h1>
       <Link href="/meal/add">Add a meal</Link>
+
+      <Calendar selected={date} onSelect={setDate} className="" />
     </div>
   );
 }
